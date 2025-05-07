9b41c50238fc309d2a435c96a5af4fad03109bc5
varunriyer <vs7835@srmist.edu.in>
2025-05-07 14:10:15 +0530

    Add function demonstrating named parameters in TypeScript

--- TypeScript_Basics.ts
+++ TypeScript_Basics.ts
@@ -318,3 +318,10 @@
     console.log(pow(3, 4));
 
 }
+{
+    //Named Parameters 
+    function divide({ dividend, divisor }: { dividend: number, divisor: number }) {
+        return dividend / divisor;
+    }
+    console.log(divide({ dividend: 10, divisor: 2 }));
+}
